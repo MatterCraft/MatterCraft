bdaf21d0e5d83facb296ec2cc582eb17af32eed2
KingDevNL <jasper@kingdev.nl>
2017-08-12 11:59:40 +0200

    MatterFabricator Block

---
+++ src/main/java/nl/kingdev/mattercraft/block/BlockMatterFabricator.java
@@ -0,0 +1,31 @@
+package nl.kingdev.mattercraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import nl.kingdev.mattercraft.MatterCraft;
+import nl.kingdev.mattercraft.info.Reference;
+
+public class BlockMatterFabricator extends Block {
+
+
+    public BlockMatterFabricator() {
+        super(Material.IRON);
+        setUnlocalizedName("Matter Fabricator");
+        setRegistryName(new ResourceLocation(Reference.mod_id, "matterfabricator"));
+        setCreativeTab(MatterCraft.blocks);
+
+    }
+
+
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        entityIn.attackEntityFrom(DamageSource.cactus, 2f);
+    }
+
+}
