85601608bffd378036fd8329b24f310cd1ac4c64
KingDevNL <jasper@kingdev.nl>
2017-08-12 12:29:19 +0200

    Organize imports

--- src/main/java/nl/kingdev/mattercraft/block/BlockMatterFabricator.java
+++ src/main/java/nl/kingdev/mattercraft/block/BlockMatterFabricator.java
@@ -2,12 +2,7 @@ package nl.kingdev.mattercraft.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import nl.kingdev.mattercraft.MatterCraft;
 import nl.kingdev.mattercraft.info.Reference;
 
