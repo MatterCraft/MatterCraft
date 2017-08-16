99d4f4267b0f535952f6e851abc83b233aaccc72
CJMinecraft01 <speed2pc2@gmail.com>
2017-08-16 13:14:12 +0100

    Converted text to language file

--- src/main/java/nl/kingdev/mattercraft/item/ItemDebugWrench.java
+++ src/main/java/nl/kingdev/mattercraft/item/ItemDebugWrench.java
@@ -1,6 +1,7 @@
 package nl.kingdev.mattercraft.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -23,67 +24,98 @@ import net.minecraftforge.items.IItemHandler;
 import nl.kingdev.mattercraft.MatterCraft;
 import nl.kingdev.mattercraft.info.Reference;
 import nl.kingdev.mattercraft.util.EnergyUtils;
+import nl.kingdev.mattercraft.util.Utils;
 
 import java.text.NumberFormat;
 
 public class ItemDebugWrench extends Item {
 
-    public ItemDebugWrench() {
-        this.setUnlocalizedName("debug_wrench");
-        this.setRegistryName(new ResourceLocation(Reference.mod_id, "debug_wrench"));
-        this.setMaxStackSize(1);
-        this.setCreativeTab(MatterCraft.items);
-    }
+	public ItemDebugWrench() {
+		this.setUnlocalizedName("debug_wrench");
+		this.setRegistryName(new ResourceLocation(Reference.mod_id, "debug_wrench"));
+		this.setMaxStackSize(1);
+		this.setCreativeTab(MatterCraft.items);
+	}
 
-    @Override
-    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        Style blue = new Style().setColor(TextFormatting.AQUA);
-        Style green = new Style().setColor(TextFormatting.GREEN);
-        if (!worldIn.isRemote) {
-            if (!playerIn.isSneaking() || playerIn.isSpectator()) return EnumActionResult.PASS;
-            if (worldIn.getBlockState(pos).getBlock() != null) {
-                Block b = worldIn.getBlockState(pos).getBlock();
-                playerIn.addChatMessage(new TextComponentString("[DEBUG] Block " + b.getRegistryName() + " @ " + pos.toString()).setStyle(blue));
-                playerIn.addChatMessage(new TextComponentString("Redstone Info:").setStyle(blue));
-                playerIn.addChatMessage(new TextComponentString("Is Powered: " + worldIn.isBlockPowered(pos) + ", Signal Strength: " + worldIn.getRedstonePower(pos, facing) + ", Is Power Source: " + b.canProvidePower(worldIn.getBlockState(pos))).setStyle(green));
-                if (worldIn.getTileEntity(pos) != null) {
-                    playerIn.addChatMessage(new TextComponentString("TileEntity Info:").setStyle(blue));
+	@Override
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos,
+			EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		Style blue = new Style().setColor(TextFormatting.AQUA);
+		Style green = new Style().setColor(TextFormatting.GREEN);
+		if (!worldIn.isRemote) {
+			if (!playerIn.isSneaking() || playerIn.isSpectator())
+				return EnumActionResult.PASS;
+			if (worldIn.getBlockState(pos).getBlock() != null) {
+				Block b = worldIn.getBlockState(pos).getBlock();
+				playerIn.addChatMessage(new TextComponentString(
+						I18n.format("item.debug_wrench.info.block", b.getRegistryName(), pos.toString()))
+								.setStyle(blue));
+				playerIn.addChatMessage(
+						new TextComponentString(I18n.format("item.debug_wrench.info.redstone1")).setStyle(blue));
+				playerIn.addChatMessage(new TextComponentString(
+						I18n.format("item.debug_wrench.info.redstone2", worldIn.isBlockPowered(pos),
+								worldIn.getRedstonePower(pos, facing), b.canProvidePower(worldIn.getBlockState(pos))))
+										.setStyle(green));
+				if (worldIn.getTileEntity(pos) != null) {
+					playerIn.addChatMessage(
+							new TextComponentString(I18n.format("item.debug_wrench.info.tileentity")).setStyle(blue));
 
-                    TileEntity t = worldIn.getTileEntity(pos);
-                    if (t.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing)) {
-                        IFluidHandler ta = t.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing);
-                        if (ta != null) {
-                            IFluidTankProperties tank = ta.getTankProperties()[0];
-                            playerIn.addChatMessage(new TextComponentString("Fluid Info:").setStyle(blue));
-                            playerIn.addChatMessage(new TextComponentString("Tank Fluid: " + tank.getContents().getLocalizedName()).setStyle(green));
-                            playerIn.addChatMessage(new TextComponentString("Tank Amount: " + NumberFormat.getInstance().format(tank.getContents().amount) + "mb / " + NumberFormat.getInstance().format(tank.getCapacity()) + "mb").setStyle(green));
-                            playerIn.addChatMessage(new TextComponentString("Can Extract: " + tank.canDrain() + ", Can Insert: " + tank.canFill()).setStyle(green));
-                        }
+					TileEntity t = worldIn.getTileEntity(pos);
+					if (t.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing)) {
+						IFluidHandler ta = t.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, facing);
+						if (ta != null) {
+							IFluidTankProperties tank = ta.getTankProperties()[0];
+							playerIn.addChatMessage(
+									new TextComponentString(I18n.format("item.debug_wrench.info.fluid1"))
+											.setStyle(blue));
+							playerIn.addChatMessage(new TextComponentString(
+									I18n.format("item.debug_wrench.info.fluid2", tank.getContents().getLocalizedName()))
+											.setStyle(green));
+							playerIn.addChatMessage(new TextComponentString(I18n.format("item.debug_wrench.info.fluid3",
+									NumberFormat.getInstance().format(tank.getContents().amount),
+									NumberFormat.getInstance().format(tank.getCapacity()))).setStyle(green));
+							playerIn.addChatMessage(new TextComponentString(
+									I18n.format("item.debug_wrench.info.fluid4", tank.canDrain(), tank.canFill()))
+											.setStyle(green));
+						}
 
-                    }
-                    if (t.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing)) {
-                        IItemHandler handler = t.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing);
-                        if (handler != null) {
-                            playerIn.addChatMessage(new TextComponentString("Inventory Info:").setStyle(blue));
-                            playerIn.addChatMessage(new TextComponentString("Slots: " + handler.getSlots()).setStyle(green));
-                            for (int i = 0; i < handler.getSlots(); i++) {
-                                ItemStack s = handler.getStackInSlot(i);
-                                if (s != null)
-                                    playerIn.addChatMessage(new TextComponentString("Slot " + i + ": " + s.toString()).setStyle(green));
-                                else
-                                    playerIn.addChatMessage(new TextComponentString("Slot " + i + " is empty!").setStyle(green));
-                            }
-                        }
-                    }
-                    if(t.hasCapability(CapabilityEnergy.ENERGY, facing)){
-                        playerIn.addChatMessage(new TextComponentString("Energy Info:").setStyle(blue));
-                        playerIn.addChatMessage(new TextComponentString("Energy Amount: " + NumberFormat.getInstance().format(EnergyUtils.getEnergyStored(t, facing)) +  " FE / " +NumberFormat.getInstance().format(EnergyUtils.getMaxEnergyStored(t, facing)) + " FE").setStyle(green));
-                        playerIn.addChatMessage(new TextComponentString("Can Extract: " + EnergyUtils.canExtract(t, facing) + ", Can Receive: " + EnergyUtils.canReceive(t, facing)).setStyle(green));
-                    }
-                }
-            }
-            return EnumActionResult.SUCCESS;
-        }
-        return EnumActionResult.PASS;
-    }
+					}
+					if (t.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing)) {
+						IItemHandler handler = t.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing);
+						if (handler != null) {
+							playerIn.addChatMessage(
+									new TextComponentString(I18n.format("item.debug_wrench.info.inventory1"))
+											.setStyle(blue));
+							playerIn.addChatMessage(new TextComponentString(
+									I18n.format("item.debug_wrench.info.inventory2", handler.getSlots()))
+											.setStyle(green));
+							for (int i = 0; i < handler.getSlots(); i++) {
+								ItemStack s = handler.getStackInSlot(i);
+								if (s != null)
+									playerIn.addChatMessage(new TextComponentString(
+											I18n.format("item.debug_wrench.info.inventory3", i, Utils.stackToString(s)))
+													.setStyle(green));
+								else
+									playerIn.addChatMessage(
+											new TextComponentString(I18n.format("item.debug_wrench.info.inventory4", i))
+													.setStyle(green));
+							}
+						}
+					}
+					if (t.hasCapability(CapabilityEnergy.ENERGY, facing)) {
+						playerIn.addChatMessage(
+								new TextComponentString(I18n.format("item.debug_wrench.info.energy1")).setStyle(blue));
+						playerIn.addChatMessage(new TextComponentString(I18n.format("item.debug_wrench.info.energy2",
+								NumberFormat.getInstance().format(EnergyUtils.getEnergyStored(t, facing)),
+								NumberFormat.getInstance().format(EnergyUtils.getMaxEnergyStored(t, facing))))
+										.setStyle(green));
+						playerIn.addChatMessage(new TextComponentString(I18n.format("item.debug_wrench.info.energy3",
+								EnergyUtils.canExtract(t, facing), EnergyUtils.canReceive(t, facing))).setStyle(green));
+					}
+				}
+			}
+			return EnumActionResult.SUCCESS;
+		}
+		return EnumActionResult.PASS;
+	}
 }
--- src/main/java/nl/kingdev/mattercraft/util/Utils.java
+++ src/main/java/nl/kingdev/mattercraft/util/Utils.java
@@ -1,8 +1,10 @@
 package nl.kingdev.mattercraft.util;
 
-import nl.kingdev.mattercraft.info.Reference;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.minecraft.item.ItemStack;
+import nl.kingdev.mattercraft.info.Reference;
 
 public class Utils {
 
@@ -14,5 +16,22 @@ public class Utils {
         }
         return logger;
     }
+    
+	/**
+	 * Converts the given {@link ItemStack} to a nice looking string
+	 * 
+	 * @param stack
+	 *            The {@link ItemStack} to convert to a string
+	 * @return The {@link ItemStack} as a string
+	 */
+	public static String stackToString(ItemStack stack) {
+		String string = (stack.stackSize / 64 != 0 ? stack.stackSize / 64 + "x64" : "x")
+				+ ((stack.stackSize - ((stack.stackSize / 64) * 64)) != 0
+						? " + " + (stack.stackSize - ((stack.stackSize / 64) * 64)) : "")
+				+ " " + stack.getDisplayName();
+		if ((stack.stackSize / 64) == 0)
+			string = string.replace(" + ", "");
+		return string;
+	}
 
 }
